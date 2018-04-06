3a46568cf79be8f045f04687260ff481e8e5f251
Jesse Sheehan <jesse@sheehan.nz>
2018-04-06 15:08:58 +1200

    Added some helper functions for the text user interface.

--- src/game/ui/text/TextUserInterfaceHelpers.java
+++ src/game/ui/text/TextUserInterfaceHelpers.java
@@ -1,15 +1,43 @@
 package game.ui.text;
 
+import java.util.Scanner;
+
 public final class TextUserInterfaceHelpers {
 	
 	public static void showMessage(String message) {
 		
-		
+		System.out.println(message);
 		
 	}
 	
-	public static int showChoice(String prompt, String[] options) {
-		return null;
+	public static boolean showYesNo(String question) {
+		
+	}
+	
+	public static int showChoice(String message, String[] options) {
+		
+		System.out.println(message);
+		
+		for (int i = 0; i < options.length; i++) {
+			
+			System.out.println(String.format("  %d) %s", i + 1, options[i]));
+			
+		}
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("> ");
+		int choice = scanner.nextInt();
+		
+		while (choice < 1 || choice >= options.length) {
+
+			System.out.print("> ");
+			choice = scanner.nextInt();
+			
+		}
+		
+		return choice - 1;
+		
 	}
 	
 	/**
