efefd963fd9d9b4abda901d01bd111f6f5dc9655
Jesse Sheehan <jesse@sheehan.nz>
2018-04-06 14:45:19 +1200

    Added some user interface classes.

---
+++ src/game/ui/text/TextUserInterface.java
@@ -0,0 +1,7 @@
+package game.ui.text;
+
+import game.UserInterface;
+
+public class TextUserInterface extends UserInterface {
+
+}
---
+++ src/game/ui/text/TextUserInterfaceHelpers.java
@@ -0,0 +1,22 @@
+package game.ui.text;
+
+public final class TextUserInterfaceHelpers {
+	
+	public static void showMessage(String message) {
+		
+		
+		
+	}
+	
+	public static int showChoice(String prompt, String[] options) {
+		return null;
+	}
+	
+	/**
+	 * Clears the screen.
+	 */
+	public static void clear() {
+		
+	}
+	
+}
