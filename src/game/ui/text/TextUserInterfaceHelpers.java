72238f457940fdfa11c60ff94e7406594aeeac88
Jesse Sheehan <jesse@sheehan.nz>
2018-04-09 10:55:07 +1200

    Added a readLine static function.

--- src/game/ui/text/TextUserInterfaceHelpers.java
+++ src/game/ui/text/TextUserInterfaceHelpers.java
@@ -1,5 +1,9 @@
 package game.ui.text;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public final class TextUserInterfaceHelpers {
@@ -37,6 +41,38 @@ public final class TextUserInterfaceHelpers {
 		}
 		
 		return choice - 1;
+		
+	}
+	
+	/**
+	 * Returns one line of text from the standard input.
+	 * @return
+	 */
+	public static String readLine() {
+		
+		// Create a new buffer of a particular size. This size denotes how many characters of input can be accepted.
+		// 1024 is probably overkill and 32 would probably suffice.
+		int BUFFER_SIZE = 1024;
+		byte[] buffer = new byte[BUFFER_SIZE];
+		
+		try {
+			
+			// Read the bytes from the System.in
+			int bytesRead = System.in.read(buffer);
+			
+			// Create a ByteBuffer from the byte array and convert to a UTF-8 CharBuffer
+			ByteBuffer byteBuffer = ByteBuffer.wrap(buffer, 0, bytesRead);
+			CharBuffer charBuffer = Charset.forName("UTF-8").decode(byteBuffer);
+			
+			// Return the resulting String
+			return charBuffer.toString();
+			
+		} catch (IOException e) {
+			
+			// If an error occurs, just return an empty String
+			return "";
+			
+		}
 		
 	}
 	
