c9695a80c9611403dc85ac36c48eb32ea717ed0a
Jesse Sheehan <jesse@sheehan.nz>
2018-04-09 15:57:45 +1200

    Added some tests for padLeft and padRight in TextUserInterfaceHelpersTest

--- src/game/ui/text/test/TextUserInterfaceHelpersTest.java
+++ src/game/ui/text/test/TextUserInterfaceHelpersTest.java
@@ -1,8 +1,10 @@
-package game.ui.text;
+package game.ui.text.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import game.ui.text.TextUserInterfaceHelpers;
 
 class TextUserInterfaceHelpersTest extends TextUserInterfaceHelpers {
 
@@ -20,6 +22,24 @@ class TextUserInterfaceHelpersTest extends TextUserInterfaceHelpers {
 		assertEquals(repeatString(str, 2), "HiHi");
 		
 		assertEquals(repeatString(str, 5), "HiHiHiHiHi");
+		
+	}
+	
+	@Test
+	void testPadLeft() {
+		
+		assertEquals(padLeft("1", ' ', 4), "   1");
+		
+		assertEquals(padLeft("7", '0', 3), "007");
+		
+		assertEquals(padLeft("> Important!", '=', 20), "========> Important!");
+		
+	}
+	
+	@Test
+	void testPadRight() {
+		
+		assertEquals(padRight("It's Over 9", '0', 14), "It's Over 9000");
 		
 	}
 
