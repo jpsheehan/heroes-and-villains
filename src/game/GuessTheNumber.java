be7b891057dec5d5ca90984367026bb057c5ca93
Jesse Sheehan <jesse@sheehan.nz>
2018-04-09 15:23:12 +1200

    Implemented a guess the number game (including demo).

--- src/game/GuessTheNumber.java
+++ src/game/GuessTheNumber.java
@@ -1,41 +1,226 @@
 package game;
 
-public class GuessTheNumber extends Minigame {
+import java.util.Random;
 
+import game.ui.text.TextUserInterfaceHelpers;
+
+public class GuessTheNumber extends Minigame<Integer, Integer, GuessTheNumberAnswer> {
+	
+	private int guessesRemaining = 2;
+	private int maxNumber = 10;
+	private int secretNumber;
+	
+	private int previousGuess;
+	private GuessTheNumberAnswer previousAnswer;
+
+	public GuessTheNumber(Ability[] abilities) {
+		super(abilities);
+		
+		// Add more guesses if applicable.
+		if (hasAbility(ItemAbility.FOUR_ATTEMPTS_AT_GUESS_THE_NUMBER)) {
+			
+			guessesRemaining = 4;
+			
+		}
+		
+		// Lower the maximum number if applicable.
+		if (hasAbility(HeroAbility.IMPROVED_ODDS)) {
+			
+			maxNumber = 9;
+			
+		}
+		
+		// Generate the secret number.
+		secretNumber = generateRandomNumber();
+	}
+	
+	/**
+	 * Returns a random integer between 1 and 10 (inclusive).
+	 * @return
+	 */
+	private Integer generateRandomNumber() {
+		
+		Random rand = new Random();
+		int number = rand.nextInt(maxNumber) + 1;
+		
+		return number;
+		
+	}
+
+	/**
+	 * 
+	 */
 	@Override
-	public void initialise(Ability[] abilities) {
-		// TODO Auto-generated method stub
+	public void doTurn(Integer choice) {
+		
+		if (state == MinigameState.PLAYING) {
+			
+			guessesRemaining--;
+			
+			// If the number has been guessed, set the state to reflect that.
+			if (choice == secretNumber) {
+				
+				state = MinigameState.WON;
+				previousAnswer = GuessTheNumberAnswer.CORRECT;
+				
+			} else {
+				
+				if (choice < secretNumber) {
+					
+					previousAnswer = GuessTheNumberAnswer.TOO_LOW;
+					
+				} else {
+					
+					previousAnswer = GuessTheNumberAnswer.TOO_HIGH;
+					
+				}
+				
+			}
+			
+			// If there are no more moves left and the game has not yet been won, the game has been lost.
+			if (guessesRemaining < 1 && state == MinigameState.PLAYING) {
+				
+				state = MinigameState.LOST;
+				
+			}
+			
+		} else {
+			
+			throw new Error("Cannot call doTurn on a game that has finished.");
+			
+		}
 
 	}
 
+	/**
+	 * Returns the last number the Hero guessed.
+	 */
 	@Override
-	public MinigameState getState() {
-		// TODO Auto-generated method stub
-		return null;
+	public Integer getHeroLastTurn() {
+		return previousGuess;
 	}
 
+	/**
+	 * Returns the result of the previous guess.
+	 */
 	@Override
-	public void doTurn(Object choice) {
-		// TODO Auto-generated method stub
-
+	public GuessTheNumberAnswer getVillainLastTurn() {
+		return previousAnswer;
 	}
 
-	@Override
-	public Object getHeroLastTurn() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getVillainLastTurn() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * Returns the number of guesses the Hero has left.
+	 */
 	@Override
 	public int getRemainingTurns() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if (state == MinigameState.PLAYING) {
+			
+			return guessesRemaining;
+			
+		} else {
+			
+			return 0;
+			
+		}
+	}
+
+	/**
+	 * Returns MinigameType.GUESS_THE_NUMBER.
+	 */
+	@Override
+	public MinigameType getType() {
+		return MinigameType.GUESS_THE_NUMBER;
+	}
+	
+	/**
+	 * Returns the maximum number that can be guessed.
+	 * @return
+	 */
+	public Integer getMaxNumber() {
+		return maxNumber;
+	}
+	
+	/**
+	 * Returns the minimum number that can be guessed (always 1).
+	 * @return
+	 */
+	public Integer getMinNumber() {
+		return 1;
+	}
+	
+	/**
+	 * Returns the secret number if the game is over.
+	 * @return
+	 */
+	public Integer getSecretNumber() {
+		
+		if (state != MinigameState.PLAYING) {
+			
+			return secretNumber;
+			
+		} else {
+			
+			throw new Error("You can't see the secret number without finishing the game!");
+			
+		}
+		
+	}
+	
+	/**
+	 * Runs a test of the GuessTheNumber game.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Ability[] abilities = new Ability[] {
+				
+		};
+		
+		GuessTheNumber game = new GuessTheNumber(abilities);
+		
+		System.out.println(String.format("Guess a number between %d and %d:", game.getMinNumber(), game.getMaxNumber()));
+		
+		while (game.getState() == MinigameState.PLAYING) {
+			
+			int guess;
+			
+			guess = TextUserInterfaceHelpers.getNumberWithBounds(game.getMinNumber(), game.getMaxNumber());
+			
+			game.doTurn(guess);
+			
+			if (game.getVillainLastTurn() == GuessTheNumberAnswer.TOO_HIGH) {
+				
+				System.out.println("Your answer was too high!");
+				
+			} else {
+				
+				if (game.getVillainLastTurn() == GuessTheNumberAnswer.TOO_LOW) {
+					
+					System.out.println("Your answer was too low!");
+					
+				} else {
+					
+					System.out.println("Your answer was right!");
+					
+				}
+				
+			}
+			
+		}
+		
+		if (game.getState() == MinigameState.WON) {
+			
+			System.out.println("You won the game!");
+			
+		} else {
+			
+			System.out.println(String.format("You lost the game! The number was %d.", game.getSecretNumber()));
+			
+		}
+		
+		
+		
 	}
 
 }
---
+++ src/game/GuessTheNumberAnswer.java
@@ -0,0 +1,14 @@
+package game;
+
+/**
+ * Denotes whether the player guessed too high, too low or correctly.
+ * @author jesse
+ *
+ */
+public enum GuessTheNumberAnswer {
+
+	TOO_HIGH,
+	TOO_LOW,
+	CORRECT
+	
+}
