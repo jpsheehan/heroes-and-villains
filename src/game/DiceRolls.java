c36518f58b48c70ed39233ad7c2e127774917ce5
Jesse Sheehan <jesse@sheehan.nz>
2018-04-09 12:12:46 +1200

    Implemented the Dice Rolls game.

--- src/game/DiceRolls.java
+++ src/game/DiceRolls.java
@@ -1,41 +1,129 @@
 package game;
 
+import java.util.Random;
+
 public class DiceRolls extends Minigame {
-
-	@Override
-	public void initialise(Ability[] abilities) {
-		// TODO Auto-generated method stub
-
+	
+	private int heroRoll;
+	private int villainRoll;
+	
+	public DiceRolls(Ability[] abilities) {
+		super(abilities);
 	}
 
-	@Override
-	public MinigameState getState() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	/**
+	 * Returns the result of a fair dice roll.
+	 * @return
+	 */
+	private int rollDice() {
 
+		Random rand = new Random();
+		return rand.nextInt(6) + 1;
+		
+	}
+	
 	@Override
 	public void doTurn(Object choice) {
-		// TODO Auto-generated method stub
+		
+		if (state == MinigameState.PLAYING) {
+			
+			heroRoll = rollDice();
+			villainRoll = rollDice();
+			
+			// Improve the odds of the hero winning.
+			// Take one away from the villains dice roll.
+			// If that doesn't work, add one to the hero's dice roll.
+			// Make sure that the resulting numbers are actual dice numbers.
+			if (hasAbility(HeroAbility.IMPROVED_ODDS)) {
+				
+				villainRoll -= 1;
+				
+				if (villainRoll < 1) {
+					villainRoll = 1;
+					
+					heroRoll += 1;
+					
+					if (heroRoll > 6) {
+						heroRoll = 6;
+					}
+				}
+				
+			}
+			
+			// The hero has won if they rolled higher than the villain
+			if (heroRoll > villainRoll) {
+				
+				state = MinigameState.WON;
+				
+			} else {
+				
+				if (heroRoll == villainRoll) {
+					
+					// If the hero rolls the same as the villain and they have the WIN_ON_DRAW ability, the hero wins.
+					if (hasAbility(ItemAbility.WIN_ON_DRAW)) {
+						
+						state = MinigameState.WON;
+						
+					} else {
+						
+						state = MinigameState.DRAWN;
+						
+					}
+					
+				} else {
+					
+					state = MinigameState.LOST;
+					
+				}
+				
+			}
+			
+			
+		} else {
+			
+			// Can't call doTurn on a game that has already finished.
+			throw new Error("Can't call doTurn on a game that has already finished.");
+			
+		}
 
 	}
 
 	@Override
 	public Object getHeroLastTurn() {
-		// TODO Auto-generated method stub
-		return null;
+		return heroRoll;
 	}
 
 	@Override
 	public Object getVillainLastTurn() {
-		// TODO Auto-generated method stub
-		return null;
+		return villainRoll;
 	}
 
 	@Override
 	public int getRemainingTurns() {
-		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public MinigameType getType() {
+		return MinigameType.DICE_ROLLS;
+	}
+	
+	public static void main(String[] args) {
+		
+		// Add the abilities that you want to start the game with here:
+		Ability[] abilities = new Ability[] {
+			// HeroAbility.IMPROVED_ODDS,
+			// ItemAbility.WIN_ON_DRAW
+		};
+		
+		DiceRolls game = new DiceRolls(abilities);
+		
+		game.doTurn(null);
+		
+		System.out.println(String.format("Your Hero rolled a %d!", (int)game.getHeroLastTurn()));
+		System.out.println(String.format("The Villain rolled a %d!", (int)game.getVillainLastTurn()));
+		System.out.println(String.format("The game was %s!", game.getState().toString().toLowerCase()));
+		
 	}
 
 }
