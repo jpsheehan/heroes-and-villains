4f0270e2c2f88e89c12b9066af335b828074618d
Manu H <mfb31@uclive.ac.nz>
2018-04-09 23:51:56 +1200

    Now, PaperScissorsRock: implemented not tested.

--- src/game/PaperScissorsRock.java
+++ src/game/PaperScissorsRock.java
@@ -1,27 +1,103 @@
 package game;
 
-public class PaperScissorsRock extends Minigame {
+import java.util.Random;
 
+public class PaperScissorsRock extends Minigame<String, String, String> {
+
+	/**
+	 * Creates a new PaperScissorsRock game
+	 * Paper beats rock, Scissors beat paper, Rock beats Scissors.
+	 * @param abilities The abilities to include in the game.
+	 */
 	public PaperScissorsRock(Ability[] abilities) {
 		super(abilities);
+		availableChoices[0] = "Paper";
+		availableChoices[1] = "Scissors";
+		availableChoices[2] = "Rock";
+	}
+
+	/**
+	 * String array contains game options: paper, scissors, rock
+	 */
+	// what about a Joker?
+	private String[] availableChoices = new String[3];
+	
+	/**
+	 * Random number generator for villains choice
+	 */
+	private Random rand = new Random();
+		
+	/**
+	 * The game option the hero chooses.
+	 */
+	private String heroChoice;
+		
+	/**
+	 * The game option the villain chooses.
+	 */
+	private String villainChoice = availableChoices[rand.nextInt(2)];
+	
+	@Override
+	public void doTurn(String choice) {
+		heroChoice = choice;
+		if (choice == null) {
+			
+			throw new IllegalArgumentException("choice should not be null for PaperScissorsRock game.");
+		}
+		if (state == MinigameState.PLAYING) {
+			
+			//currently these are hardcoded, would be better to make variables / constants
+			if (heroChoice == "Paper") {
+				if (villainChoice == "Scissors") {
+					state = MinigameState.LOST;
+				}
+				if (villainChoice == "Rock") {
+					state = MinigameState.WON;
+				}
+			}
+			
+			if (heroChoice == "Scissors") {
+				if (villainChoice == "Rock") {
+					state = MinigameState.LOST;
+				}
+				if (villainChoice == "Paper") {
+					state = MinigameState.WON;
+				}
+			}
+			
+			if (heroChoice == "Rock") {
+				if (villainChoice == "Paper") {
+					state = MinigameState.LOST;
+				}
+				if (villainChoice == "Scissors") {
+					state = MinigameState.WON;
+				}
+			}
+			
+			if (heroChoice == villainChoice) {
+				
+				// If the hero choice is the same as the villain and they have the WIN_ON_DRAW ability, the hero wins.
+				if (hasAbility(ItemAbility.WIN_ON_DRAW)) {
+					
+					state = MinigameState.WON;	
+				} 
+				else {		
+					state = MinigameState.DRAWN;
+				}
+			}
+		}
 	}
 
 	@Override
-	public void doTurn(Object choice) {
+	public String getHeroLastTurn() {
 		// TODO Auto-generated method stub
-
+		return heroChoice;
 	}
 
 	@Override
-	public Object getHeroLastTurn() {
+	public String getVillainLastTurn() {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object getVillainLastTurn() {
-		// TODO Auto-generated method stub
-		return null;
+		return villainChoice;
 	}
 
 	@Override
