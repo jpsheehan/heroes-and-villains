d1ac4dfb5b0fb64b75ae47a53fa82da93df181ae
jesse <jesse@localhost>
2018-04-11 14:06:54 +1200

    Created methods for global random state.

--- src/game/GeneralHelpers.java
+++ src/game/GeneralHelpers.java
@@ -6,6 +6,38 @@ import java.util.Random;
 public final class GeneralHelpers {
 
 	/**
+	 * The random state used for the game.
+	 */
+	private static Random randomState = null;
+	
+	/**
+	 * Seeds the random state with a particular number (Useful for testing)
+	 * @param seed The seed to use.
+	 */
+	public static void seedRandom(long seed) {
+		
+		GeneralHelpers.randomState = new Random();
+		randomState.setSeed(seed);
+		
+	}
+	
+	/**
+	 * Gets the random state for the game.
+	 * @return
+	 */
+	public static Random getRandom() {
+		
+		if (GeneralHelpers.randomState == null) {
+			
+			GeneralHelpers.randomState = new Random();
+			
+		}
+		
+		return GeneralHelpers.randomState;
+		
+	}
+	
+	/**
 	 * Shuffles an ArrayList (poorly).
 	 * @param list The list to shuffle.
 	 * @return Returns a copy of the list shuffled.
