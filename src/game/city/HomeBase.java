bc281f74adea00154938fd0e7c8622026b9f3f34
jesse <jesse@localhost>
2018-04-11 12:02:44 +1200

    Added HomeBase and PowerUpDen classes to game.city

---
+++ src/game/city/HomeBase.java
@@ -0,0 +1,13 @@
+package game.city;
+
+public class HomeBase extends Area {
+
+	/**
+	 * Creates a new Home Base.
+	 * @param name The name of the Home Base.
+	 */
+	public HomeBase(String name) {
+		super(name, AreaType.HOME_BASE);
+	}
+
+}
---
+++ src/game/city/PowerUpDen.java
@@ -0,0 +1,13 @@
+package game.city;
+
+public class PowerUpDen extends Area {
+
+	/**
+	 * Creates a new Power Up Den.
+	 * @param name The name of the area.
+	 */
+	public PowerUpDen(String name) {
+		super(name, AreaType.POWER_UP_DEN);
+	}
+
+}
