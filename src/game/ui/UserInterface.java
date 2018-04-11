d2d4fc79d3d17191190531d3f0f162cd85ea0fed
jesse <jesse@localhost>
2018-04-11 12:18:59 +1200

    Added some abstract methods to game.ui.UserInterface.

--- src/game/ui/UserInterface.java
+++ src/game/ui/UserInterface.java
@@ -2,4 +2,10 @@ package game.ui;
 
 public abstract class UserInterface {
 
+	public abstract void showTitleScreen();
+	
+	public abstract void showGameCreationScreen();
+	
+	public abstract void showTeamCreationScreen();
+	
 }
--- src/game/ui/text/TextUserInterface.java
+++ src/game/ui/text/TextUserInterface.java
@@ -4,4 +4,22 @@ import game.ui.UserInterface;
 
 public class TextUserInterface extends UserInterface {
 
+	@Override
+	public void showTitleScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showGameCreationScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showTeamCreationScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
