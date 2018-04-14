4ef32728b2a923d5063d3ee1cffeb6e3050a8896
Manu H <mfb31@uclive.ac.nz>
2018-04-15 00:26:12 +1200

    Created VillainsLair

---
+++ src/game/city/VillainsLair.java
@@ -0,0 +1,13 @@
+package game.city;
+
+public class VillainsLair extends Area {
+
+	/**
+	 * Creates a new Villains Lair.
+	 * @param name The name of the Home Base.
+	 */
+	public VillainsLair(String name) {
+		super(name, AreaType.VILLAINS_LAIR);
+	}
+
+}
