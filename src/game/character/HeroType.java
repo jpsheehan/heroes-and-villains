2825e06b85efd231a02f603f0b5f554e83f3e2cf
Jesse Sheehan <jesse@sheehan.nz>
2018-05-02 09:05:51 +1200

    Added a getAbility method to the HeroType enumeration.

--- src/game/character/HeroType.java
+++ src/game/character/HeroType.java
@@ -44,9 +44,46 @@ public enum HeroType {
 				return getString("HeroType.Science");
 			
 			default:
-				return "null";
+				throw new AssertionError();
 		
 		}
 		
 	}
+	
+	/**
+	 * Returns the HeroAbility of the HeroType.
+	 * @return
+	 */
+	public HeroAbility getAbility() {
+		
+		switch (this) {
+			
+			case ARTS_STUDENT:
+				return HeroAbility.THROW_MONEY;
+				
+			case COMMERCE_STUDENT:
+				return HeroAbility.CHEAPER_ITEMS;
+				
+			case COMPUTER_SCIENCE_STUDENT:
+				return HeroAbility.HACK_MAINFRAME;
+				
+			case ENGINEERING_STUDENT:
+				return HeroAbility.DAMAGE_REDUCTION;
+				
+			case LAW_STUDENT:
+				return HeroAbility.PREVENTS_ROBBERY;
+				
+			case MATHS_STUDENT:
+				return HeroAbility.IMPROVED_ODDS;
+				
+			case SCIENCE_STUDENT:
+				return HeroAbility.INCREASED_RECOVERY_RATE;
+			
+			default:
+				throw new AssertionError();
+				
+		}
+		
+	}
+	
 }
