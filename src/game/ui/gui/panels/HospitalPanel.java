1a73eca75634aab16df1c18581f4b0cbd265de80
Jesse Sheehan <jesse@sheehan.nz>
2018-05-13 16:07:34 +1200

    Started adding components to HospitalPanel (unfinished)

--- src/game/ui/gui/panels/HospitalPanel.java
+++ src/game/ui/gui/panels/HospitalPanel.java
@@ -2,7 +2,23 @@ package game.ui.gui.panels;
 
 import javax.swing.JPanel;
 
+import game.Team;
+import game.character.Hero;
+import game.ui.gui.DialogResult;
 import game.ui.gui.Triggerable;
+import game.ui.gui.dialogs.HeroSelectionDialog;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import game.item.HealingItem;
+import game.item.Inventory;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.FlowLayout;
 
 public class HospitalPanel extends JPanel {
 
@@ -12,14 +28,120 @@ public class HospitalPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = -6330847966216959076L;
+	
+	private Hero selectedHero;
+	private HealingItem selectedItem;
+	
+	private JButton btnApplyHealingItem;
+	private JLabel lblSelectedItem, lblSelectedHero;
 
 	/**
 	 * Create the panel.
 	 */
-	public HospitalPanel(Triggerable window) {
+	public HospitalPanel(Triggerable window, Team team) {
 
 		this.window = window;
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		
+		JPanel panel_6 = new JPanel();
+		add(panel_6);
+		panel_6.setLayout(new BoxLayout(panel_6, BoxLayout.Y_AXIS));
+		
+		JPanel panel = new JPanel();
+		panel_6.add(panel);
+		
+		JLabel lblThisIsA = new JLabel("This is a hospital. You can apply healing items here.");
+		panel.add(lblThisIsA);
+		
+		JPanel panel_1 = new JPanel();
+		
+		JButton btnSelectAHero = new JButton("Select a Hero...");
+		btnSelectAHero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				HeroSelectionDialog dlg = new HeroSelectionDialog(team.getHeroes());
+				dlg.setVisible(true);
+				
+				if (dlg.getDialogResult() == DialogResult.OK) {
+					
+					selectedHero = dlg.getSelectedHero();
+					
+				}
+				
+			}
+		});
+		
+		JPanel panel_2 = new JPanel();
+		panel_6.add(panel_2);
+		
+		JLabel lblLeadingHero = new JLabel("Selected Hero:");
+		panel_2.add(lblLeadingHero);
+		
+		lblSelectedHero = new JLabel("lblSelectedHero");
+		panel_2.add(lblSelectedHero);
+		panel_1.add(btnSelectAHero);
+		panel_6.add(panel_1);
+		
+		JPanel panel_3 = new JPanel();
+		panel_6.add(panel_3);
+		
+		JLabel lblSelectedItem_1 = new JLabel("Selected Item:");
+		panel_3.add(lblSelectedItem_1);
+		
+		lblSelectedItem = new JLabel("lblSelectedItem");
+		panel_3.add(lblSelectedItem);
+		
+		JPanel panel_4 = new JPanel();
+		panel_6.add(panel_4);
+		
+		JButton btnSelectAnItem = new JButton("Select an Item...");
+		btnSelectAnItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				
+			}
+		});
+		panel_4.add(btnSelectAnItem);
+		
+		JPanel panel_5 = new JPanel();
+		panel_6.add(panel_5);
+		
+		btnApplyHealingItem = new JButton("Apply Healing Item");
+		panel_5.add(btnApplyHealingItem);
+		btnApplyHealingItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+				
+			}
+		});
 		
 	}
-
+	
+	public void update() {
+		
+		if (selectedItem == null) {
+			
+			lblSelectedItem.setText("None");
+			
+		} else {
+			
+			lblSelectedItem.setText(selectedItem.getName());
+			
+		}
+		
+		if (selectedHero == null) {
+			
+			lblSelectedHero.setText("None");
+			
+		} else {
+			
+			lblSelectedHero.setText(selectedHero.getName());
+			
+		}
+		
+		btnApplyHealingItem.setEnabled(selectedHero != null && selectedItem != null);
+		
+	}
 }
