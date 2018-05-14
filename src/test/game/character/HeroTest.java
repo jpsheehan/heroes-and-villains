8fed440c52b2fa8a0597aea6ad533fc75a77bc48
mfb31 <mfb31@cs18259kq.canterbury.ac.nz>
2018-05-14 14:39:37 +1200

    Matched up with updated hero ability (WITTY_PHRASES)

--- src/test/game/character/HeroTest.java
+++ src/test/game/character/HeroTest.java
@@ -43,7 +43,7 @@ class HeroTest {
 	@Test
 	void testGetAbility() {
 		assertEquals(HeroAbility.IMPROVED_ODDS, hero_1.getAbility());
-		assertEquals(HeroAbility.THROW_MONEY, hero_2.getAbility());
+		assertEquals(HeroAbility.WITTY_PHRASES, hero_2.getAbility());
 		assertEquals(HeroAbility.CHEAPER_ITEMS, hero_3.getAbility());
 		assertEquals(HeroAbility.HACK_MAINFRAME, hero_4.getAbility());
 		assertEquals(HeroAbility.DAMAGE_REDUCTION, hero_5.getAbility());
