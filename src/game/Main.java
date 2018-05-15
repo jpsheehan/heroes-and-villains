93a1b4ecd8446cbe2fde9e4e84149cdbfd38d731
Jesse Sheehan <jps111@uclive.ac.nz>
2018-05-16 11:19:12 +1200

    Altered random seed to give a "Guess the Number" game in the first city.

--- src/game/Main.java
+++ src/game/Main.java
@@ -6,7 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		GeneralHelpers.seedRandom(4L);
+		// 0L gives paper scissors rock in first building.
+		// 3L gives guess the number in first building. 
+		// 4L gives dice rolls in first building.
+		final long seed = 3L;
+		
+		GeneralHelpers.seedRandom(seed);
 		new GameEnvironment(GraphicalUserInterface.class).run();
 
 	}
