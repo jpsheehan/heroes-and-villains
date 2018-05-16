f275a5a933f1cb3302bedc8431b9e686be9c323f
mfb31 <mfb31@cs18283kq.canterbury.ac.nz>
2018-05-16 17:08:05 +1200

    Guess btn disabled if 0 remaining turns. trigger(GameEvent) not working.

--- src/game/ui/gui/panels/games/PanelGuessTheNumberGame.java
+++ src/game/ui/gui/panels/games/PanelGuessTheNumberGame.java
@@ -8,6 +8,7 @@ import game.GeneralHelpers;
 import game.Team;
 import game.character.Hero;
 import game.ui.gui.DialogResult;
+import game.ui.gui.GameEvent;
 import game.ui.gui.Triggerable;
 import game.ui.gui.dialogs.HeroSelectionDialog;
 import game.ui.gui.dialogs.ItemSelectionDialog;
@@ -37,33 +38,30 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
 
-public class PanelGuessTheNumberGame extends GenericAreaPanel {
+public class PanelGuessTheNumberGame extends GenericAreaPanel implements Triggerable {
 
 	/**
 	 * 
 	 */
 
 	private static final long serialVersionUID = -5763958068575027679L;
-	private int diceRollingLoop = 10;
-	private JLabel lblVillainRoll;
-	private JLabel lblHeroRoll;
+	private Triggerable villainsLairPanel;
+	//private int completedAnimations;
 	private GuessTheNumber guessTheNumber;
-	//private BattleScreen battleScreen;
 	JLabel lblWinner;
-	JButton button;
+	JButton btnGuess;
 	private final JSlider slider = new JSlider();
 	
 	/**
 	 * Create the panel.
 	 */
-	public PanelGuessTheNumberGame(Triggerable window, BattleScreen battleScreen) {
+	public PanelGuessTheNumberGame(Triggerable villainsLairPanel, BattleScreen battleScreen) {
 		
+		this.villainsLairPanel = villainsLairPanel;
 		guessTheNumber = (GuessTheNumber)battleScreen.getMinigame();
 		
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		JLabel lblHeroDice;
-		JLabel lblVillainDice;
-		String lblVillainDiceValue = "0";
+		//String lblVillainDiceValue = "0";
 		
 		JPanel panel_6 = new JPanel();
 		add(panel_6);
@@ -83,28 +81,16 @@ public class PanelGuessTheNumberGame extends GenericAreaPanel {
 		
 		JLabel lblClickStartToRoll = new JLabel("Move slider, then click \"Guess\" to start game");
 		panel_2.add(lblClickStartToRoll);
+		
 		panel_6.add(panel_1);
 		slider.setValue(3);
 		slider.setMajorTickSpacing(1);
 		slider.setPaintLabels(true);
 		slider.setPaintTicks(true);
 		slider.setSnapToTicks(true);
-		slider.setMinimum(1);
-		slider.setMaximum(6);
+		slider.setMinimum(guessTheNumber.getMinNumber());
+		slider.setMaximum(guessTheNumber.getMaxNumber());
 		panel_1.add(slider);
-		
-		//TODO
-		/*\
-		 Event handler to to detect button press
-		 set the heroGuess to the number clicked
-		 update the HeroGuess label value
-		 for loop with timer to get and display dice values 
-		 then update the VillainRoll to the final dice value
-		 set the winner
-		 set the ok button viable 
-		 */
-		
-		//do we need a turns left label?
 		
 		JPanel panel_3 = new JPanel();
 		panel_6.add(panel_3);
@@ -115,67 +101,63 @@ public class PanelGuessTheNumberGame extends GenericAreaPanel {
 		JPanel panel_4 = new JPanel();
 		panel_6.add(panel_4);
 		
-		lblHeroDice = new JLabel("lblHeroDiceValue");
-		panel_4.add(lblHeroDice);
-		lblHeroDice.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblHeroDice.setHorizontalAlignment(SwingConstants.LEFT);
+		JLabel lblTurnsLeftText = new JLabel("Turns left:");
+		panel_4.add(lblTurnsLeftText);
 		
-		lblVillainDice = new JLabel("lblVillainDiceValue");
-		panel_4.add(lblVillainDice);
-		lblVillainDice.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-		lblVillainDice.setHorizontalAlignment(SwingConstants.RIGHT);
+		JLabel lblTurnsLeft = new JLabel("Turns Left");
+		panel_4.add(lblTurnsLeft);
+		lblTurnsLeft.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTurnsLeft.setEnabled(false);
 		
-		JButton btnGuess = new JButton("Guess");
+		JLabel lblGuessText = new JLabel("Hero Guess:");
+		panel_5.add(lblGuessText);
+		
+		JLabel lblHeroGuess = new JLabel("Hero Guess");
+		panel_5.add(lblHeroGuess);
+		lblHeroGuess.setHorizontalAlignment(SwingConstants.LEFT);
+		lblHeroGuess.setEnabled(false);
+		
+		JPanel panel_7 = new JPanel();
+		panel_6.add(panel_7);
+		
+		JLabel lblGuessResult = new JLabel("Guess High / Low / Correct");
+		panel_7.add(lblGuessResult);
+		lblGuessResult.setEnabled(false);
+		
+		btnGuess = new JButton("Guess");
 		btnGuess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblHeroDice.setText((String.format("%d", slider.getValue())));
-				for (int i = 0; i < diceRollingLoop ; i++) {
-					
-					//get dice values and display on labels
-					lblVillainDice.setText(String.format("%d", GeneralHelpers.getRandom().nextInt(6) + 1));
-					
-					//pause 0.1 seconds
-					//(new Timer(100, null)).start();
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException e1) {
-						// TODO Auto-generated catch block
-						//e.printStackTrace();
-					}
+				lblHeroGuess.setText((String.format("%d", slider.getValue())));
+				guessTheNumber.doTurn(slider.getValue());
+				lblTurnsLeft.setText(new Integer(guessTheNumber.getRemainingTurns()).toString());
+				lblTurnsLeft.setEnabled(true);
+				lblHeroGuess.setEnabled(true);
+				
+				switch (guessTheNumber.getVillainLastTurn()) {
+						case TOO_HIGH:
+							lblGuessResult.setText("Hero guess too high!");
+							break;
+						case TOO_LOW:
+							lblGuessResult.setText("Hero guess too low!");
+							break;
+						case CORRECT:
+							lblGuessResult.setText("Hero guess correct!");
+							break;
+						default:
+							//
 				}
-				lblVillainDice.setText(guessTheNumber.getHeroLastTurn().toString());
+				lblGuessResult.setEnabled(true);
+				
+				//disable the guess button if run out of turns
+				if (guessTheNumber.getRemainingTurns() == 0) {
+					disableAll();
+				}
 			}
 		});
+		
 		btnGuess.setActionCommand("OK");
 		panel_3.add(btnGuess);
 		
-		
-		JLabel lblHeroRoll_1 = new JLabel("Hero Guess          ");
-		panel_5.add(lblHeroRoll_1);
-		lblHeroRoll_1.setHorizontalAlignment(SwingConstants.LEFT);
-		
-		JLabel lblVillainRoll_1 = new JLabel("      Villain Roll");
-		panel_5.add(lblVillainRoll_1);
-		lblVillainRoll_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		
-		JPanel panel_7 = new JPanel();
-		panel_7.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
-		panel_6.add(panel_7);
-		
-		button = new JButton("OK");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//TODO
-				System.out.println("Exit this panel.");
-			}
-		});
-		button.setEnabled(false);
-		panel_7.add(button);
-		button.setActionCommand("OK");
 	}
 	
 	public void update() {
@@ -201,6 +183,51 @@ public class PanelGuessTheNumberGame extends GenericAreaPanel {
 			break;
 		default:
 			lblWinner.setText("Unknown");
+		}
+		
+	}
+	
+	private void disableAll() {
+		
+		btnGuess.setEnabled(false);
+		
+	}
+	
+	@Override
+	public void trigger(GameEvent event) {
+		
+		if (event == GameEvent.ANIMATION_COMPLETE) {
+			
+				// Check the status of the game here and update components/trigger events in the villainsLairPanel
+				switch (guessTheNumber.getState()) {
+					
+					case WON:
+						disableAll();
+						villainsLairPanel.trigger(GameEvent.MINIGAME_WON);
+						break;
+						
+					case LOST:
+						disableAll();
+						villainsLairPanel.trigger(GameEvent.MINIGAME_LOST);
+						break;
+					
+					case DRAWN:
+						//should never be a draw
+						villainsLairPanel.trigger(GameEvent.MINIGAME_DRAWN);
+						break;
+						
+					case PLAYING:
+						System.out.println(guessTheNumber.getState());
+						break;
+						
+					default:
+						throw new AssertionError();
+				
+			}
+			
+			// The parent must handle the event
+			villainsLairPanel.trigger(event);
+			
 		}
 		
 	}
