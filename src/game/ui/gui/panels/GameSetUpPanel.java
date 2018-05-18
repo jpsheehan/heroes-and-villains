e2dffeb2ae6ebc202ad7f4bb33a6dbbbfbb7e357
manuh <manuh@10.196.12.5>
2018-05-18 21:18:11 +1200

    new class GameSetUpPanel

---
+++ src/game/ui/gui/panels/GameSetUpPanel.java
@@ -0,0 +1,19 @@
+package game.ui.gui.panels;
+
+import javax.swing.JPanel;
+
+public class GameSetUpPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6625880562455508713L;
+
+	/**
+	 * Create the panel.
+	 */
+	public GameSetUpPanel() {
+
+	}
+
+}
