87b66435f6e136aadef011639682a77363ede419
Jesse Sheehan <jesse@sheehan.nz>
2018-05-22 10:02:13 +1200

    Removed random seed.

--- src/game/Main.java
+++ src/game/Main.java
@@ -15,12 +15,6 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		// 0L gives paper scissors rock in first building.
-		// 3L gives guess the number in first building. 
-		// 4L gives dice rolls in first building.
-		final long seed = 0L;
-		
-		GeneralHelpers.seedRandom(seed);
 		GraphicalUserInterface gui = new GraphicalUserInterface(true);
 		gui.start();
 
