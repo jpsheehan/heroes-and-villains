bc3bc573c9e3b0dcf5c90c9d99117d0424643733
Jesse Sheehan <jesse@sheehan.nz>
2018-05-25 14:45:10 +1200

    Tests.

--- src/test/game/city/HomeBaseTest.java
+++ src/test/game/city/HomeBaseTest.java
@@ -2,13 +2,40 @@ package test.game.city;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import game.city.AreaType;
+import game.city.HomeBase;
 
 class HomeBaseTest {
 
+	private HomeBase homeBase;
+	
+	@BeforeEach
+	void beforeEach() {
+		homeBase = new HomeBase("Name", "Description");
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetName() {
+		
+		assertEquals("Name", homeBase.getName());
+		
+	}
+	
+	@Test
+	void testGetFlavourText() {
+		
+		assertEquals("Description", homeBase.getFlavourText());
+		
+	}
+	
+	@Test
+	void testGetType() {
+		
+		assertEquals(AreaType.HOME_BASE, homeBase.getType());
+		
 	}
 
 }
