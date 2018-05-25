838bdbc926ca971b977fe409901ac99d91abdbe5
mfb31 <mfb31@cs18235kq.canterbury.ac.nz>
2018-05-25 12:55:13 +1200

    HeroAbility test

--- src/test/game/character/HeroAbilityTest.java
+++ src/test/game/character/HeroAbilityTest.java
@@ -2,12 +2,30 @@ package test.game.character;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import game.character.HeroAbility;
+import game.character.HeroType;
+
 
 class HeroAbilityTest {
 
+	private HeroAbility commerce, engineering, arts, maths, law, compsci;
+	
+	@BeforeAll
+	static void beforeAll(){
+		
+	}
+	
+	
 	@Test
 	void testToString() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void getFlavourText() {
 		fail("Not yet implemented");
 	}
 
