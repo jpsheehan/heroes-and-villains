e491a6b56171ec9937f178ad794b6ac6572b5fe3
Jesse Sheehan <jesse@sheehan.nz>
2018-05-25 14:42:01 +1200

    Removed some tests.
    Added more tests.

--- src/test/game/city/ShopTest.java
+++ src/test/game/city/ShopTest.java
@@ -2,13 +2,63 @@ package test.game.city;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import game.character.Dialogue;
+import game.character.InnKeeper;
+import game.city.AreaType;
+import game.city.CityType;
+import game.city.Shop;
+import game.item.Item;
+import game.item.Map;
 
 class ShopTest {
 
+	Shop shop;
+	
+	@BeforeEach
+	void beforeEach() {
+		ArrayList<Item> items = new ArrayList<Item>();
+		items.add(new Map("Map", "Description", 0));
+		shop = new Shop("Name", "Flavour", items, new InnKeeper("Steve", new Dialogue(CityType.ENG_CORE)));
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetName() {
+		
+		assertEquals("Name", shop.getName());
+		
+	}
+	
+	@Test
+	void testGetFlavourText() {
+		
+		assertEquals("Flavour", shop.getFlavourText());
+		
+	}
+	
+	@Test
+	void testGetType() {
+		
+		assertEquals(AreaType.SHOP, shop.getType());
+		
+	}
+	
+	@Test
+	void testGetInnKeeper() {
+		
+		assertEquals("Steve", shop.getInnKeeper().getName());
+		
+	}
+	
+	@Test
+	void testGetInventory() {
+		
+		assertEquals(0, shop.getInventory().size());
+		
 	}
 
 }
